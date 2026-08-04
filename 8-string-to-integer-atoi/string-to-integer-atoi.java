8da8fba419ddb07494ae97d312697b1e5bab1325
Mohita Nagar <mohita.24bce11412@vitbhopal.ac.in>
2026-08-04 22:48:15 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (88.90%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        int i = 0;
+        int n = s.length();
+
+        // Step 1: Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Step 2: Check sign
+        int sign = 1;
+
+        if (i < n && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (i < n && s.charAt(i) == '+') {
+            i++;
+        }
+
+        int num = 0;
+
+        // Step 3: Read digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+
+            int digit = s.charAt(i) - '0';
+
+            // Step 4: Overflow check
+            if (num > Integer.MAX_VALUE / 10 ||
+               (num == Integer.MAX_VALUE / 10 && digit > 7)) {
+
+                return sign == 1
+                        ? Integer.MAX_VALUE
+                        : Integer.MIN_VALUE;
+            }
+
+            num = num * 10 + digit;
+            i++;
+        }
+
+        return sign * num;
+    }
+}
