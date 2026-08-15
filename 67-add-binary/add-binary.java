dbfa92420cb4dcb177083c07e2cca9c842ec46ec
Mohita Nagar <mohita.24bce11412@vitbhopal.ac.in>
2026-08-15 19:15:26 +0530

    Time: 5 ms (8.88%) | Memory: 45 MB (6.89%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        String result = "";
+
+        while(i >= 0 || j >= 0 || carry != 0){
+            int sum = carry;
+
+            if( i>= 0){
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if( j >= 0){
+                sum += b.charAt(j) - '0';
+                j--;
+
+            }
+            
+            int digit = (sum %2);
+            carry = sum/2;
+
+            result = digit + result;
+
+            
+                
+        }
+   
+       return result;
+
+    
+    }
+}
