5e3d476ead6a0377b6a819a77304bc4a38fb60d8
Mohita Nagar <mohita.24bce11412@vitbhopal.ac.in>
2026-08-22 21:38:37 +0530

    Time: 3 ms (31.03%) | Memory: 49.2 MB (7.28%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits.length() == 0) {
+            return result;
+        }
+
+        String[] phone = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        backtrack(digits, 0, "", result, phone);
+
+        return result;
+    }
+
+    private void backtrack(String digits, int index, String current,
+                            List<String> result, String[] phone) {
+
+        // All digits processed
+        if (index == digits.length()) {
+            result.add(current);
+            return;
+        }
+
+        // Get letters corresponding to current digit
+        String letters = phone[digits.charAt(index) - '0'];
+
+        // Try every possible letter
+        for (char ch : letters.toCharArray()) {
+            backtrack(digits, index + 1, current + ch, result, phone);
+        }
+    }
+}
